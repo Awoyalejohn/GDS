00816580e142911a0ffd4a0eaec67dc2f6f4601f
Awoyalejohn <awoyalejohn@gmail.com>
2022-07-22 19:34:49 +0100

    change Django project name to graphicsdesignspace

--- graphicsdesignspace/asgi.py
+++ graphicsdesignspace/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for gds project.
+ASGI config for graphicsdesignspace project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gds.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'graphicsdesignspace.settings')
 
 application = get_asgi_application()
