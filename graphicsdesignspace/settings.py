03692615d5a649a8537f876b130dee36eda0b50e
Awoyalejohn <awoyalejohn@gmail.com>
2022-07-22 20:42:55 +0100

    Add a conditional if statement to use the Postgres database

--- graphicsdesignspace/settings.py
+++ graphicsdesignspace/settings.py
@@ -78,15 +78,16 @@ WSGI_APPLICATION = 'graphicsdesignspace.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.getenv('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
