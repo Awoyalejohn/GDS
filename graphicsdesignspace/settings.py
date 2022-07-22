ae410379a7d0e61216660a54e45991b8ae34f8ee
Awoyalejohn <awoyalejohn@gmail.com>
2022-07-22 21:00:24 +0100

    Add HTTP_HOST header to fix runserver command issue

--- graphicsdesignspace/settings.py
+++ graphicsdesignspace/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['graphics-design-space.herokuapp.com']
+ALLOWED_HOSTS = ['graphics-design-space.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
