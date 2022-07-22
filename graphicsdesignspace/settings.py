f622331672d05a3486f0bb09ab3caa8a8b6bc1a0
Awoyalejohn <awoyalejohn@gmail.com>
2022-07-22 21:46:54 +0100

    Add directories for the static and media folders

--- graphicsdesignspace/settings.py
+++ graphicsdesignspace/settings.py
@@ -125,7 +125,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
