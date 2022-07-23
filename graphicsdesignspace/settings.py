ffc7849cc3ba3f055a3473e6e59cfc47ddb9f40b
Awoyalejohn <awoyalejohn@gmail.com>
2022-07-23 08:55:14 +0100

    Add authentication backends for allauth

--- graphicsdesignspace/settings.py
+++ graphicsdesignspace/settings.py
@@ -64,12 +64,20 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request',
+                'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
         },
     },
+]
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
 WSGI_APPLICATION = 'graphicsdesignspace.wsgi.application'
