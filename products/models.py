8cc67f5274553e26f5bf14495d21bde34f9f2753
Awoyalejohn <awoyalejohn@gmail.com>
2022-07-24 22:29:16 +0100

    Add a verbose naem plural for category model

--- products/models.py
+++ products/models.py
@@ -4,8 +4,12 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=254)
 
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
     def __str__(self):
         return self.name
+
 
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
