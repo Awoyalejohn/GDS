99f0f66f6d7f56dc6cf0a17e688fdbdad2ecdf7f
Awoyalejohn <awoyalejohn@gmail.com>
2022-07-24 22:49:36 +0100

    Add produvt list view and add its path to urls.py

--- products/views.py
+++ products/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from products.models import Product
 
 # Create your views here.
+class ProductListView(ListView):
+    """ A view to list all products """
+    model = Product
