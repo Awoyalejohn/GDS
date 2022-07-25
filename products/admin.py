1756b9b6ab93e694bf56e7381e389f55a7f007ee
Awoyalejohn <awoyalejohn@gmail.com>
2022-07-25 22:25:03 +0100

    customise admin to show more information for the models and add prepoluated slug field

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,13 @@
+from atexit import register
 from django.contrib import admin
 from products.models import Category, Product
 
 # Register your models here.
 admin.site.register(Category)
-admin.site.register(Product)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'category', 'price')
+    prepopulated_fields = {"slug": ("name",)}
+
+
