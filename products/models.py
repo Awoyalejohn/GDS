2703c3cc82bb6c65e73ba5a403c6384f8c2136a6
Awoyalejohn <awoyalejohn@gmail.com>
2022-07-25 21:38:04 +0100

    Add slug field to products model and change max length

--- products/models.py
+++ products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-    name = models.CharField(max_length=254)
+    name = models.CharField(max_length=250, unique=True)
 
     class Meta:
         verbose_name_plural = 'Categories'
@@ -13,7 +13,8 @@ class Category(models.Model):
 
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    name = models.CharField(max_length=254)
+    name = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True, null=True, blank=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     # rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
