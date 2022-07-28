a2439f54944c20b2f13cccc4c9c7186b2ff47fa5
Awoyalejohn <awoyalejohn@gmail.com>
2022-07-28 16:22:35 +0100

    Add view and url path for product detail page

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from products.views import ProductListView
+from products.views import ProductListView, ProductDetailView
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='products'),
+    path('<slug:slug>', ProductDetailView.as_view(), name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,7 +1,12 @@
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from products.models import Product
 
 # Create your views here.
 class ProductListView(ListView):
+    """ A view to list all products """
+    model = Product
+
+class ProductDetailView(DetailView):
     """ A view to list all products """
     model = Product
