42b423d71378b6baf636fd0e71253d4c8a8d0628
Awoyalejohn <awoyalejohn@gmail.com>
2022-07-29 11:01:41 +0100

    Change product list view to a generic view and add a new path for template dirs

--- products/views.py
+++ products/views.py
@@ -1,12 +1,21 @@
-from django.views.generic.list import ListView
+from django.shortcuts import render
+from django.views.generic import View
 from django.views.generic.detail import DetailView
 from products.models import Product
 
 # Create your views here.
-class ProductListView(ListView):
+class ProductListView(View):
     """ A view to list all products """
-    model = Product
+    def get(self, request):
+        products = Product.objects.all()
+        context = {'product_list': products}
+        return render(request, 'products/product_list.html', context)
+
+
+
 
 class ProductDetailView(DetailView):
-    """ A view to display and individual item's product page """
+    """ 
+    A view to display and individual item's product page
+    """
     model = Product
