e0a3702372dd83d098af43ab744898e919774647
Awoyalejohn <awoyalejohn@gmail.com>
2022-07-29 18:38:03 +0100

    change the products app models fields for slug and category to be required

--- products/models.py
+++ products/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=250, unique=True)
-    slug = models.SlugField(max_length=250, unique=True, null=True, blank=True)
+    slug = models.SlugField(max_length=250, unique=True)
 
     class Meta:
         verbose_name_plural = 'Categories'
@@ -13,9 +13,9 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
     name = models.CharField(max_length=250, unique=True)
-    slug = models.SlugField(max_length=250, unique=True, null=True, blank=True)
+    slug = models.SlugField(max_length=250, unique=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
