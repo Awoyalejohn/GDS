690e320bab3254080110bad480bf3c69cf35d33f
Awoyalejohn <awoyalejohn@gmail.com>
2022-07-29 16:19:03 +0100

    Add slug field to Category model and add list display for it in the admin

--- products/admin.py
+++ products/admin.py
@@ -3,7 +3,10 @@ from django.contrib import admin
 from products.models import Category, Product
 
 # Register your models here.
-admin.site.register(Category)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {"slug": ("name",)}
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
--- products/models.py
+++ products/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True, null=True, blank=True)
 
     class Meta:
         verbose_name_plural = 'Categories'
