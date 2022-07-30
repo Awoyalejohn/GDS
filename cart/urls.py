0ed137b90ef7feade6acd2f6b27e25583baa7137
Awoyalejohn <awoyalejohn@gmail.com>
2022-07-30 18:42:28 +0100

    Add add to cart functionality

--- cart/urls.py
+++ cart/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from cart.views import DisplayCartView
+from cart.views import DisplayCartView, AddToCart
 
 urlpatterns = [
     path('', DisplayCartView.as_view(), name='display_cart'),
+    path('add/<slug:slug>/', AddToCart.as_view(), name='add_to_cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,6 @@
+from django.shortcuts import redirect
+from django.contrib import messages
+from django.views.generic import View
 from django.views.generic import TemplateView
 
 # Create your views here.
@@ -6,3 +9,29 @@ class DisplayCartView(TemplateView):
     """ A view that displays the contents of the cart """
     
     template_name = "cart/cart.html"
+
+
+class AddToCart(View):
+    """ A view to add items to the cart page """
+    def post(self, request, slug):
+        quantity = 1
+        #gets the redirect url
+        redirect_url = request.POST.get('redirect_url')
+        #checks if the cart is in the session and then creates an empty dict if it isn't
+        cart = request.session.get('cart', {})
+        
+        #checks if the slug is in the cart dictionary
+        if slug in list(cart.keys()):
+            messages.error(request, "You already added that item to the cart!")
+            return redirect(redirect_url)
+            
+        else:
+            cart[slug] = quantity
+
+        #Adds the amount of that item to its value in the dictionary
+        request.session['cart'] = cart
+        
+        print(request.session['cart'])
+        return redirect(redirect_url)
+        
+    
