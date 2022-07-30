e5411d4aa88c2dc782c653451d209b978a044d63
Awoyalejohn <awoyalejohn@gmail.com>
2022-07-30 19:30:41 +0100

    Update context processor to loop through the items in the cart and fix dicount delta bug

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,16 +1,31 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 def cart_contents(request):
     cart_items = []
     subtotal = 0
     product_count = 0
+    cart = request.session.get('cart',{})
 
-    if subtotal < settings.DISCOUNT_THRESHOLD:
+    for slug, quantity in cart.items():
+        product = get_object_or_404(Product, slug=slug)
+        subtotal += product.price
+        product_count += quantity
+        cart_items.append({
+            'slug': slug,
+            'quantity': quantity,
+            'product': product,
+        })
+
+    if subtotal > settings.DISCOUNT_THRESHOLD:
+        discount = subtotal * Decimal(settings.DISCOUNT_PERCENTAGE / 100)
+        discount_delta = 0
+        
+    else:
         discount = 0
         discount_delta = settings.DISCOUNT_THRESHOLD - subtotal
-    else:
-        discount = subtotal * Decimal(settings.DISCOUNT_PERCENTAGE / 100)
 
     total = subtotal - discount
 
