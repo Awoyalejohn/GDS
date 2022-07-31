0e4d30ca3db31f6b0d077a900e9eca46e2f7c1e1
Awoyalejohn <awoyalejohn@gmail.com>
2022-07-31 18:13:21 +0100

    Change query selector for getting product items to get_objector404 and add error message for failure to remove an item from cart

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, HttpResponse
+from django.shortcuts import get_object_or_404, redirect, HttpResponse
 from django.contrib import messages
 from products.models import Product
 from django.views.generic import View
@@ -15,7 +15,7 @@ class DisplayCartView(TemplateView):
 class AddToCart(View):
     """ A view to add items to the cart page """
     def post(self, request, slug):
-        product = Product.objects.get(slug=slug)
+        product = get_object_or_404(Product, slug=slug)
         quantity = 1
         #gets the redirect url
         redirect_url = request.POST.get('redirect_url')
@@ -24,7 +24,7 @@ class AddToCart(View):
         
         #checks if the slug is in the cart dictionary
         if slug in list(cart.keys()):
-            messages.error(request, "You already added that item to the cart!")
+            messages.error(request, f"You already added {product.name} to your cart!")
             return redirect(redirect_url)
             
         else:
@@ -42,15 +42,18 @@ class RemoveFromCart(View):
     """ A view to remove an item from the cart page """
     def post(self, request, slug):
         try:
+            product = get_object_or_404(Product, slug=slug)
             #checks if the cart is in the session and then creates an empty dict if it isn't
             cart = request.session.get('cart', {})
 
             #deletes the item in the list
             cart.pop(slug)
+            messages.success(request, f'Removed {product.name} from your cart')
 
             request.session['cart'] = cart
             return HttpResponse(status=200)
         except Exception as e:
+            messages.error(request, f'Error removing item: {e}')
             return HttpResponse(status=500)
 
     
