56d279066aa224919a33f4d584e2e21509ac77ba
Awoyalejohn <awoyalejohn@gmail.com>
2022-07-31 16:04:07 +0100

    Add remove from cart view and to enable removing items from the shopping bag

--- cart/urls.py
+++ cart/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from cart.views import DisplayCartView, AddToCart
+from cart.views import DisplayCartView, AddToCart, RemoveFromCart
 
 urlpatterns = [
     path('', DisplayCartView.as_view(), name='display_cart'),
     path('add/<slug:slug>/', AddToCart.as_view(), name='add_to_cart'),
+    path('remove/<slug:slug>/', RemoveFromCart.as_view(), name='remove_from_cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect
+from django.shortcuts import redirect, HttpResponse
 from django.contrib import messages
 from django.views.generic import View
 from django.views.generic import TemplateView
@@ -33,5 +33,21 @@ class AddToCart(View):
         
         print(request.session['cart'])
         return redirect(redirect_url)
-        
+
+
+class RemoveFromCart(View):
+    """ A view to remove an item from the cart page """
+    def post(self, request, slug):
+        try:
+            #checks if the cart is in the session and then creates an empty dict if it isn't
+            cart = request.session.get('cart', {})
+
+            #deletes the item in the list
+            cart.pop(slug)
+
+            request.session['cart'] = cart
+            return HttpResponse(status=200)
+        except Exception as e:
+            return HttpResponse(status=500)
+
     
