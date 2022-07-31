22e1b91ae12c7a54b405cb3b6b23d067da0fcd95
Awoyalejohn <awoyalejohn@gmail.com>
2022-07-31 21:12:22 +0100

    Create checkout app and models

---
+++ checkout/models.py
@@ -0,0 +1,82 @@
+import uuid
+
+from django.db import models
+from django.db.models import Sum
+from django.conf import settings
+
+# from django_countries.fields import CountryField
+from products.models import Product
+# from profiles.models import UserProfile
+
+
+
+# Create your models here.
+class Order(models.Model):
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    # user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+    #                                  null=True, blank=True, related_name='orders')
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    # country = CountryField(blank_label='Country *', null=False, blank=False)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    discount = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
+    order_subtotal = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    # original_cart = models.TextField(null=False, blank=False, default='')
+    # stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
+
+
+    def _generate_order_number(self):
+        """
+        Generate a random, unique order number using UUID
+        """
+        return uuid.uuid4().hex.upper()
+
+    def update_total(self):
+        """
+        Update total each time a line item is added,
+        accounting for discounts
+        """
+        self.order_subtotal = self.lineitems.aggregate(Sum('lineitem_subtotal'))['lineitem_subtotal__sum'] or 0
+        if self.order_subtotal > settings.DISCOUNT_THRESHOLD:
+            self.discount = self.order_subtotal * settings.DISCOUNT_PERCENTAGE / 100
+        else:
+            self.discount = 0
+        self.total = self.subtotal - self.discount
+        self.save()
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the order number
+        if it hasn't been set already.
+        """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
+
+  
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    lineitem_subtotal = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the lineitem subtotal
+        and update the order total.
+        """
+        self.lineitem_subtotal = self.product.price
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'order {self.order.order_number}'
