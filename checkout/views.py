143822c3fc3f9ff810099a2392cc382fcd83ccc8
Awoyalejohn <awoyalejohn@gmail.com>
2022-08-03 12:32:42 +0100

    Add post method for checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -3,6 +3,8 @@ from django.contrib import messages
 from django.conf import settings
 from django.views.generic import View
 from .forms import OrderForm
+from .models import Order, OrderLineItem
+from products.models import Product
 from cart.contexts import cart_contents
 
 import stripe
@@ -10,7 +12,7 @@ import stripe
 # Create your views here.
 class Checkout(View):
     """ A view to allow purchasing of the items from the checkout page """
-    def post(self, request):
+    def get(self, request):
         stripe_public_key = settings.STRIPE_PUBLIC_KEY
         stripe_secret_key = settings.STRIPE_SECRET_KEY
 
@@ -45,3 +47,42 @@ class Checkout(View):
         }
 
         return render(request, template, context)
+
+
+    def post(self, request):
+        cart = request.session.get('cart', {})
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'country': request.POST['country'],
+            'postcode': request.POST['postcode'],
+            'town_or_city': request.POST['town_or_city'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'county': request.POST['county'],
+        }
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            order = order_form.save()
+            for slug in cart.items():
+                try:
+                    product = Product.objects.get(slug=slug)
+                    order_line_item = OrderLineItem(
+                        order=order,
+                        product=product,
+                    )
+                    order_line_item.save()
+                except Product.DoesNotExist:
+                    messages.error(request, (
+                        "One of the products in your bag wasn't found in our database. "
+                        "Please call us for assistance!")
+                    )
+                    order.delete()
+                    return redirect(reverse('view_cart'))
+
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_success', args=[order.order_number]))
+        else:
+            messages.error(request, 'There was an error with your form. \
+                Please double check your information.')
