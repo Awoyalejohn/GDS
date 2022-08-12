fc01ba0f67152a611e05da712cdf71a1608b22ee
Awoyalejohn <awoyalejohn@gmail.com>
2022-08-12 19:43:53 +0100

    Update functionality for multiple images to be converted into a blob to get downloaded on same site

--- checkout/static/checkout/js/download.js
+++ checkout/static/checkout/js/download.js
@@ -1,14 +1,18 @@
-let downloadItem = document.querySelector('.download-item');
-let url = downloadItem.getAttribute('href');
+// Now we need it to work for all items
+const downloadItems = document.querySelectorAll('.download-item');
+for (let downloadItem of downloadItems ) {
+    let url = downloadItem.getAttribute('href');
 
-fetch(url)
-  .then(res => res.blob()) // Gets the response and returns it as a blob
-  .then(blob => {
-    // Here's where you get access to the blob
-    // And you can use it for whatever you want
-    // Like calling ref().put(blob)
+    fetch(url)
+    .then(res => res.blob()) // Gets the response and returns it as a blob
+    .then(blob => {
+        // Here's where you get access to the blob
+        // And you can use it for whatever you want
+        // Like calling ref().put(blob)
 
-    // Here, I use it to make an image appear on the page
-    let objectURL = URL.createObjectURL(blob);
-    downloadItem.setAttribute("href", objectURL); 
-});
+        // Here, I use it to make an image appear on the page
+        let objectURL = URL.createObjectURL(blob);
+        downloadItem.setAttribute("href", objectURL); 
+    });
+
+}
