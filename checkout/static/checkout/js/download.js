7e6b9fc8b86d62088efca096cd3bdffe8567228d
Awoyalejohn <awoyalejohn@gmail.com>
2022-08-12 19:26:28 +0100

    Add download functionality of a single image using the fetch api to convert images to blobs, so that they can be downloaded on same site

---
+++ checkout/static/checkout/js/download.js
@@ -0,0 +1,14 @@
+let downloadItem = document.querySelector('.download-item');
+let url = downloadItem.getAttribute('href');
+
+fetch(url)
+  .then(res => res.blob()) // Gets the response and returns it as a blob
+  .then(blob => {
+    // Here's where you get access to the blob
+    // And you can use it for whatever you want
+    // Like calling ref().put(blob)
+
+    // Here, I use it to make an image appear on the page
+    let objectURL = URL.createObjectURL(blob);
+    downloadItem.setAttribute("href", objectURL); 
+});
