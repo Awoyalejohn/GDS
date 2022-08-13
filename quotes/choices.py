9fe2719932ac2539211a0d6c8bd3746934138474
Awoyalejohn <awoyalejohn@gmail.com>
2022-08-13 14:40:31 +0100

    Addd quote request model

---
+++ quotes/choices.py
@@ -0,0 +1,14 @@
+TYPE_CHOICES = [
+    ('IC', 'Icon'),
+    ('LG', 'Logo'),
+    ('PS', 'Poster'),
+    ('ST', 'Sticker'),
+    ('WP', 'Wallpaper'),
+    ('BN', 'Banner'),
+]
+
+SIZE_CHOICES = [
+    ('S', 'Small'),
+    ('M', 'Medium'),
+    ('L', 'Large'),
+]
--- quotes/models.py
+++ quotes/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from profiles.models import UserProfile
+from quotes.choices import TYPE_CHOICES, SIZE_CHOICES
+
+import uuid
 
 # Create your models here.
+class QuoteRequest(models.Model):
+    name = models.CharField(max_length=250)
+    user =  models.ForeignKey(UserProfile, on_delete=models.SET_NULL, null=True, blank=True)
+    type = models.CharField(max_length=2, choices=TYPE_CHOICES)
+    size = models.CharField(max_length=1, choices=SIZE_CHOICES)
+    description = models.TextField()
+    submitted = models.DateTimeField(auto_now_add=True)
+    quote_order_number = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+
+    def __str__(self):
+        return self.name
