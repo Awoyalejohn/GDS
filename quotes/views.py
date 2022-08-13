4d5cbfab201d311b61beebdd6b363cc9418c3a19
Awoyalejohn <awoyalejohn@gmail.com>
2022-08-13 16:09:14 +0100

    add quote request form to quote request template to view on website

--- quotes/views.py
+++ quotes/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.views.generic import View
+from django.contrib import messages
+from .forms import QuoteRequestForm
 # Create your views here.
 class QuoteRequestView(View):
     def get(self, request):
+        form = QuoteRequestForm()
         template = 'quotes/quote_request.html'
-        context = {}
+        context = {'form': form}
 
         return render(request, template, context)
+    
+    def post(self, request):
+        form = QuoteRequestForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Thank you for your request')
+            return redirect(reverse('quote_request'))
