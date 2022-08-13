a8e371c1b08e3136419513fc3735fe7f5cf37341
Awoyalejohn <awoyalejohn@gmail.com>
2022-08-13 21:00:09 +0100

    Update choice fields for quote request to display prices in select box

--- quotes/choices.py
+++ quotes/choices.py
@@ -1,14 +1,14 @@
 TYPE_CHOICES = [
-    ('IC', 'Icon'),
-    ('LG', 'Logo'),
-    ('PS', 'Poster'),
-    ('ST', 'Sticker'),
-    ('WP', 'Wallpaper'),
-    ('BN', 'Banner'),
+    ('IC', 'Icon --- £9.99'),
+    ('LG', 'Logo --- £19.99'),
+    ('PS', 'Poster --- £39.99'),
+    ('ST', 'Sticker --- £9.99'),
+    ('WP', 'Wallpaper --- £39.99'),
+    ('BN', 'Banner --- £19.99'),
 ]
 
 SIZE_CHOICES = [
-    ('S', 'Small'),
-    ('M', 'Medium'),
-    ('L', 'Large'),
+    ('S', 'Small --- £9.99'),
+    ('M', 'Medium --- £19.99'),
+    ('L', 'Large --- £29.99'),
 ]
