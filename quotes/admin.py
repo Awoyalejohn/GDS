595eb5d6b2a0af0786fbe8e01aec4ea8181c1bee
Awoyalejohn <awoyalejohn@gmail.com>
2022-08-14 16:08:18 +0100

    Create quote order model and remove order number field from quote request model

--- quotes/admin.py
+++ quotes/admin.py
@@ -4,4 +4,4 @@ from quotes.models import QuoteRequest
 # Register your models here.
 @admin.register(QuoteRequest)
 class QuoteRequestAdmin(admin.ModelAdmin):
-    list_display = ('name', 'user', 'submitted', 'quote_order_number')
+    list_display = ('name', 'user', 'submitted')
--- quotes/models.py
+++ quotes/models.py
@@ -12,7 +12,25 @@ class QuoteRequest(models.Model):
     size = models.CharField(max_length=1, choices=SIZE_CHOICES)
     description = models.TextField()
     submitted = models.DateTimeField(auto_now_add=True)
-    quote_order_number = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
 
     def __str__(self):
         return self.name
+
+
+class QuoteOrder(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, null=True, blank=True)
+    name = models.CharField(max_length=250)
+    email = models.CharField(max_length=250)
+    quote_request_name = models.CharField(max_length=250)
+    type = models.CharField(max_length=250)
+    size = models.CharField(max_length=250)
+    description = models.TextField()
+    subtotal = models.CharField(max_length=250)
+    discount = models.CharField(max_length=250)
+    total = models.CharField(max_length=250)
+    submitted = models.DateTimeField(auto_now_add=True)
+    quote_order_number = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    stripe_pid = models.CharField(max_length=250, null=False, blank=False, default='')
+
+    def __str__(self):
+        return self.quote_request_name
