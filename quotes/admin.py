7971e51a8323153e1d6acaba2d35494b822d9a7d
Awoyalejohn <awoyalejohn@gmail.com>
2022-08-14 16:10:37 +0100

    Register quote order model to admin

--- quotes/admin.py
+++ quotes/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from quotes.models import QuoteRequest
+from quotes.models import QuoteRequest, QuoteOrder
 
 # Register your models here.
 @admin.register(QuoteRequest)
 class QuoteRequestAdmin(admin.ModelAdmin):
     list_display = ('name', 'user', 'submitted')
+
+
+@admin.register(QuoteOrder)
+class QuoteOrderAdmin(admin.ModelAdmin):
+    list_display = ('quote_request_name', 'user', 'submitted')
