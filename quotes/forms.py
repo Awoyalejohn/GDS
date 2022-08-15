9faecf83784e22c98de4017b9704e42d1ddc6f32
Awoyalejohn <awoyalejohn@gmail.com>
2022-08-15 22:54:24 +0100

    Add placeholder and labels to quote order form

--- quotes/forms.py
+++ quotes/forms.py
@@ -7,6 +7,18 @@ class QuoteRequestForm(forms.ModelForm):
         model = QuoteRequest
         exclude = ('user', 'quote_request_number')
 
+        labels = {
+            'type': 'What type of design do you need?',
+            'size': 'What size?',
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(QuoteRequestForm, self).__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs['placeholder'] = 'Give the design a name'
+        self.fields['description'].widget.attrs['placeholder'] = 'Write a description here'
+    
+
+
 
 class QuoteOrderForm(forms.ModelForm):
     class Meta:
