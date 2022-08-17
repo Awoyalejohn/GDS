c64c10f862a20b37af9f2e05eeb3cf64ef3e8b75
Awoyalejohn <awoyalejohn@gmail.com>
2022-08-17 23:41:58 +0100

    Make migrations and add quote fufillment model to admin

--- quotes/admin.py
+++ quotes/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from quotes.models import QuoteRequest, QuoteOrder
+from quotes.models import QuoteRequest, QuoteOrder, QuoteFufillment
 
 # Register your models here.
 @admin.register(QuoteRequest)
@@ -13,3 +13,8 @@ class QuoteOrderAdmin(admin.ModelAdmin):
     list_display = ('quote_request_name', 'user', 'submitted', 'quote_order_number')
     readonly_fields = ('quote_request_name', 'type', 'size', 'description',
                        'subtotal', 'discount', 'total', 'quote_order_number')
+
+
+@admin.register(QuoteFufillment)
+class QuoteFufillmentAdmin(admin.ModelAdmin):
+    list_display = ('quote_order', 'image', 'status')
