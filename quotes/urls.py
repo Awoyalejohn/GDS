c3248b99fe98b52d7ef0dab4849c00f3f1e9b4c2
Awoyalejohn <awoyalejohn@gmail.com>
2022-08-18 23:23:13 +0100

    Fix error in link to post history detail template and error in the url

--- quotes/urls.py
+++ quotes/urls.py
@@ -3,7 +3,7 @@ from quotes.views import QuoteRequestView, QuoteCheckoutView, QuoteCheckoutSucce
 urlpatterns = [
     path('', QuoteRequestView.as_view(), name='quote_request'),
     path('quote_history/', QuoteHistoryView.as_view(), name='quote_history'),
-    path('quote_history/<quote_order>', QuoteHistoryDetail.as_view(), name='quote_history_detail'),
+    path('quote_history/<quote_order_number>', QuoteHistoryDetail.as_view(), name='quote_history_detail'),
     path('quote_checkout/', QuoteCheckoutView.as_view(), name='quote_checkout'),
     path('quote_checkout_success/<quote_order_number>', QuoteCheckoutSuccess.as_view(), name='quote_checkout_success'),
 ]
