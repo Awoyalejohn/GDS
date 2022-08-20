b0e44584270c11ce5612c15771a778cefeed86ce
Awoyalejohn <awoyalejohn@gmail.com>
2022-08-20 22:12:03 +0100

    Update the update review view to use a base view instead of a update view CBV

--- reviews/views.py
+++ reviews/views.py
@@ -1,40 +1,32 @@
 from django.shortcuts import render, get_object_or_404, reverse
-from django.urls import reverse_lazy
-from django.views.generic.edit import View, UpdateView, DeleteView
+from django.views.generic.edit import View
 from django.http import HttpResponseRedirect
 from .models import Review
-from products.models import Product
 from .forms import ReviewForm
 from django.contrib import messages
 
 
-# Create your views here.
+class UpdateReview(View):
+    """ A view to edit a product review """
+    def get(self, request, review_id):
+        review = get_object_or_404(Review, id=review_id)
+        slug = review.product.slug
+        print(slug)
+        form = ReviewForm(instance=review)
+        context = {'review': review, 'form':form}
+        template = 'reviews/edit_review.html'
+        return render(request, template, context)
 
-# Redirect mixin form stackoverflow post
-# https://stackoverflow.com/questions/62626660/redirect-back-to-previous-page-django
-class RedirectToPreviousMixin:
-    """ A mixin that allows users to redirect back to a previous page when form is submitted"""
-    default_redirect = '/'
+    def post(self, request, review_id):
+        review = get_object_or_404(Review, id=review_id)
+        slug = review.product.slug
+        print(slug)
+        form = ReviewForm(self.request.POST, instance=review)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Review was updated successfully')
+            return HttpResponseRedirect(reverse('product_detail', args=[slug]))
 
-    def get(self, request, *args, **kwargs):
-        request.session['previous_page'] = request.META.get('HTTP_REFERER', self.default_redirect)
-        return super().get(request, *args, **kwargs)
-
-    def get_success_url(self):
-        return self.request.session['previous_page']
-class UpdateReview(RedirectToPreviousMixin, UpdateView):
-    """ A view to edit a product review"""
-    model = Review
-    form_class = ReviewForm
-    template_name = 'reviews/edit_review.html'
-    success_message = 'Successfully Updated review!'
-    error_message = 'Failed to update review. Please ensure the form is valid.'
-
-    def get_object(self):
-        review = Review.objects.get(id=self.kwargs['review_id'])
-        return review
-
-    
 
 class DeleteReview(View):
     """ A view for users to delete reviews """
@@ -45,7 +37,6 @@ class DeleteReview(View):
         context = {'review': review}
         template = 'reviews/delete_review.html'
         return render(request, template, context)
-
 
     def post(self, request, review_id):
         review = get_object_or_404(Review, id=review_id)
