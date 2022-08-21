29839dbcd79549e476b73727b24339857c30fbfb
Awoyalejohn <awoyalejohn@gmail.com>
2022-08-21 18:57:06 +0100

    Add template  and view  to add testimonials to testimonials page

--- testimonials/urls.py
+++ testimonials/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import TestimonialListView
+from .views import TestimonialListView, AddTestimonialView
 
 urlpatterns = [
     path('', TestimonialListView.as_view(), name='testimonials'),
+    path('add_testimonial', AddTestimonialView.as_view(), name='add_testimonial'),
 ]
--- testimonials/views.py
+++ testimonials/views.py
@@ -1,10 +1,37 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.contrib import messages
 from .models import Testimonial
-from django.views.generic import ListView
+from profiles.models import UserProfile
+from .forms import TestimonialForm
+from django.views.generic import ListView, CreateView
 
 
 # Create your views here.
 class TestimonialListView(ListView):
+    """ Displays a list of all approved user testimonials """
     model = Testimonial
     template_name = 'testimonials/testimonial_list.html'
     context_object_name = 'testimonials'
+
+
+class AddTestimonialView(CreateView):
+    """ creates and adds a user testimonial to be approved for the testimonila page """
+    model = Testimonial
+    form_class = TestimonialForm
+    template_name = 'testimonials/add_testimonial.html'
+    success_message = 'Successfully added testimonial'
+    error_message = 'Failed to add testimonial. Please ensure the form is valid.'
+
+    def get_success_url(self):
+        return reverse_lazy('testimonials')
+
+    def form_valid(self, form):
+        form.instance.user = UserProfile.objects.get(user=self.request.user)
+        messages.success(self.request, self.success_message)
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, self.error_message)
+        return super().form_invalid(form)
+
