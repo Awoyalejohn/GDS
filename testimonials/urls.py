c53946506ef67a26a309faab9a67ea485d4b9b49
Awoyalejohn <awoyalejohn@gmail.com>
2022-08-21 19:15:30 +0100

    Add edit testimonial functionality

--- testimonials/urls.py
+++ testimonials/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import TestimonialListView, AddTestimonialView
+from .views import TestimonialListView, AddTestimonialView, EditTestimonialView
 
 urlpatterns = [
     path('', TestimonialListView.as_view(), name='testimonials'),
-    path('add_testimonial', AddTestimonialView.as_view(), name='add_testimonial'),
+    path('add_testimonial/', AddTestimonialView.as_view(), name='add_testimonial'),
+    path('edit_testimonial/<int:id>', EditTestimonialView.as_view(), name='edit_testimonial'),
 ]
--- testimonials/views.py
+++ testimonials/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
 from django.contrib import messages
 from .models import Testimonial
 from profiles.models import UserProfile
 from .forms import TestimonialForm
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 
 
 # Create your views here.
@@ -34,4 +34,30 @@ class AddTestimonialView(CreateView):
     def form_invalid(self, form):
         messages.error(self.request, self.error_message)
         return super().form_invalid(form)
+
+
+class EditTestimonialView(UpdateView):
+    """ A view for users to edit Testimonials """
+    model = Testimonial
+    form_class = TestimonialForm
+    template_name = 'testimonials/edit_testimonial.html'
+    success_message = 'Successfully updated testimonial'
+    error_message = 'Failed to update testimonial. Please ensure the form is valid.'
+
+    def get_object(self):
+        id = self.kwargs.get("id")
+        return get_object_or_404(Testimonial, id=id)
+
+    def get_success_url(self):
+        return reverse_lazy('testimonials')
+
+    def form_valid(self, form):
+        messages.success(self.request, self.success_message)
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, self.error_message)
+        return super().form_invalid(form)
+
+
 
