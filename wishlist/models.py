45412cea1ec6ee4e57fd3a55fb805899345a21a6
Awoyalejohn <awoyalejohn@gmail.com>
2022-08-22 21:44:36 +0100

    Add models for wishlist app

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from profiles.models import UserProfile
+from products.models import Product 
 
 # Create your models here.
+class WishList(models.Model):
+    user_profile = models.OneToOneField(UserProfile, on_delete=models.CASCADE, related_name='wish_list')
+
+
+class WishListItem(models.Model):
+    wish_list = models.ForeignKey(WishList, on_delete=models.CASCADE, related_name='wish_list_item')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='wish_list_item')
+
+    class Meta:
+        unique_together = ('wish_list', 'product')
