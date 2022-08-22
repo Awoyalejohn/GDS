ca039960dfb4e04b82453104fe31f86d75619d20
Awoyalejohn <awoyalejohn@gmail.com>
2022-08-22 22:23:46 +0100

    Register wishlist models in admin

--- wishlist/models.py
+++ wishlist/models.py
@@ -6,6 +6,9 @@ from products.models import Product
 class WishList(models.Model):
     user_profile = models.OneToOneField(UserProfile, on_delete=models.CASCADE, related_name='wish_list')
 
+    def __str__(self):
+        return f"{self.user_profile}'s Wish List"
+
 
 class WishListItem(models.Model):
     wish_list = models.ForeignKey(WishList, on_delete=models.CASCADE, related_name='wish_list_item')
