7ef1ef91ec7397fcd249a64a3d0c7473c00586f5
Awoyalejohn <awoyalejohn@gmail.com>
2022-08-23 12:08:44 +0100

    Add view to add items to wish list and add the path for it in urls.py

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import WishListView
+from .views import WishListView, AddToWishList
 
 urlpatterns = [
     path('', WishListView.as_view(), name='wish_list'),
+    path('add/<slug:slug>/', AddToWishList.as_view(), name='add_to_wish_list'),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import get_object_or_404
-from django.views.generic import TemplateView
+from django.shortcuts import get_object_or_404, redirect
+from django.views.generic import TemplateView, View
 from profiles.models import UserProfile
+from products.models import Product
+from wishlist.models import WishListItem
+from django.contrib import messages
 
 # Create your views here.
 class WishListView(TemplateView):
+    """ A view to display items in the users wishlist """
     template_name = 'wishlist/wish_list.html'
 
     def get_context_data(self, **kwargs):
@@ -12,3 +16,21 @@ class WishListView(TemplateView):
         wish_list_items = profile.wish_list.wish_list_item.all()
         context['wish_list_items'] = wish_list_items
         return context
+
+
+class AddToWishList(View):
+    """ A view to add items to the user's wishlist """
+    def post(self, request, slug):
+        profile = get_object_or_404(UserProfile, user=self.request.user)
+        product = get_object_or_404(Product, slug=slug)
+        #gets the redirect url
+        redirect_url = request.POST.get('redirect_url')
+        wish_list_item, created = WishListItem.objects.get_or_create(wish_list=profile, product=product)
+        if created:
+        # A new wislist item object created
+            messages.success(request, f"Added {product.name} to your wish list")
+        else:
+        # wishlist item object already exists
+            messages.error(request, f"Error adding item to wishlist!")
+        return redirect(redirect_url)
+
