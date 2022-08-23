8347869c201edd6812172558212aa4093eb78199
Awoyalejohn <awoyalejohn@gmail.com>
2022-08-23 17:47:08 +0100

    Add singal to create a wishlist for a user's profile when their account is made

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from profiles.models import UserProfile
 from products.models import Product 
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class WishList(models.Model):
@@ -16,3 +18,11 @@ class WishListItem(models.Model):
 
     class Meta:
         unique_together = ('wish_list', 'product')
+
+
+@receiver(post_save, sender=UserProfile)
+def create_wish_list(sender, instance, created, **kwargs):
+    """ Create a wishlist for a user's profile """
+    if created:
+        WishList.objects.create(user_profile=instance)
+    print('wishlist signal received')
