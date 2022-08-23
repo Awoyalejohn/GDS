a795fe21279e85e91cade71b6542663976f529c4
Awoyalejohn <awoyalejohn@gmail.com>
2022-08-23 11:01:36 +0100

    Change wishlist view context to wish list items and display all contents of a users wish list in the wishlist page by looping through the wishlist items

--- wishlist/views.py
+++ wishlist/views.py
@@ -9,6 +9,6 @@ class WishListView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(WishListView, self).get_context_data(**kwargs)
         profile = get_object_or_404(UserProfile, user=self.request.user)
-        wishlist = profile.wish_list.all()
-        context['wishlist'] = wishlist
+        wish_list_items = profile.wish_list.wish_list_item.all()
+        context['wish_list_items'] = wish_list_items
         return context
