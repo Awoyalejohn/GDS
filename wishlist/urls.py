2736cfad4867b4605030519bf285271c17a3c70c
Awoyalejohn <awoyalejohn@gmail.com>
2022-08-24 20:45:01 +0100

    Fix bug with  conflicting url path names for add to cart from wish list

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', WishListView.as_view(), name='wish_list'),
     path('add/<slug:slug>/', AddToWishList.as_view(), name='add_to_wish_list'),
     path('remove/<slug:slug>/', RemoveFromWishList.as_view(), name='remove_from_wish_list'),
-    path('add_to_cart/<slug:slug>/', AddToCart.as_view(), name='add_to_cart'),
+    path('add_to_cart/<slug:slug>/', AddToCart.as_view(), name='add_to_cart_from_wishlist'),
 ]
