fdd2be8f50d8ca67115802dd3aa771e4ed121af2
Awoyalejohn <awoyalejohn@gmail.com>
2022-08-26 07:12:43 +0100

    Change path to profile info page

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from profiles.views import ProfileBillingDetails, OrderHistory, OrderHistoryDetail, ProfileDownloads, ProfileInfo, ProfileReviews, ProfileTestimonials
 
 urlpatterns = [
-    path('profile_info/', ProfileInfo.as_view(), name='profile_info'),
+    path('', ProfileInfo.as_view(), name='profile_info'),
     path('profile_billing_details/', ProfileBillingDetails.as_view(), name='profile_billing_details'),
     path('order_history/', OrderHistory.as_view(), name='order_history'),
     path('order_history_detail/<order_number>', OrderHistoryDetail.as_view(), name='order_history_detail'),
