ce637903cc745d11f2c47f3ba3f84ee299f45202
Awoyalejohn <awoyalejohn@gmail.com>
2022-08-28 11:17:02 +0100

    Add 404 page

--- graphicsdesignspace/urls.py
+++ graphicsdesignspace/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,3 +32,4 @@ urlpatterns = [
     path('testimonials/', include('testimonials.urls')),
     path('wishlist/', include('wishlist.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+handler404 = 'graphicsdesignspace.views.handler404'
