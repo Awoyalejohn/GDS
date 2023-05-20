7adec01a417366ee482ba5b62b35a5d7cd9e4601
root <root@DESKTOP-QJV5AEU.localdomain>
2023-05-20 09:06:46 +0100

    Update allowed hosts settings

--- graphicsdesignspace/settings.py
+++ graphicsdesignspace/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['graphics-design-space.herokuapp.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
